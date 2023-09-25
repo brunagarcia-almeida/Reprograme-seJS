8ed03b58fb24f8b9e9f70e4f39c43271709cb17e
brunagarcia-almeida <73366397+brunagarcia-almeida@users.noreply.github.com>
2023-09-25 21:30:39 +0000

    Utilizando terminal Shell
    node ex1.js para rodar o segundo arquivo JS

---
+++ ex1.js
@@ -0,0 +1,6 @@
+//Primeiro exemplo
+var num1 = parseInt("1");
+var num2 = parseFloat("2");
+var num3 = String(3);
+
+console.log((num1 + num2) + num3);
--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-//Primeiro exemplo
-// var num1 = parseInt("1");
-// var num2 = parseFloat("2");
-// var num3 = String(3);
-
-// console.log((num1 + num2) + num3);
-
-//Segundo exemplo
+// Prática exemplo
 // var salario_bruto = 7500;
 // var ir = salario_bruto * 0.10;
 // var salario_liquido = salario_bruto - ir;
 
 // console.log("Salário líquido: R$", salario_liquido);
 
-// //Terceiro exemplo
+// Prática exemplo
 const prompt = require("prompt-sync")();
 
 var nome = prompt("Digite seu nome: ");
@@ -20,4 +13,4 @@ var salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
 var ir = salario_bruto * 0.10;
 var salario_liquido = salario_bruto - ir;
 
-console.log("Olá", nome,"! Seu salário líquido é: R$", salario_liquido);
+console.log("Olá", nome, "! Seu salário líquido é: R$", salario_liquido);
