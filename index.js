80894a1891093ea2b0ad9718fb8a9af66b913b0b
brunagarcia-almeida <73366397+brunagarcia-almeida@users.noreply.github.com>
2023-09-25 21:19:35 +0000

    Entrada de dados
    Reprograme-se IFES

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-var salario_bruto = 7500;
+//Primeiro exemplo
+// var num1 = parseInt("1");
+// var num2 = parseFloat("2");
+// var num3 = String(3);
+
+// console.log((num1 + num2) + num3);
+
+//Segundo exemplo
+// var salario_bruto = 7500;
+// var ir = salario_bruto * 0.10;
+// var salario_liquido = salario_bruto - ir;
+
+// console.log("Salário líquido: R$", salario_liquido);
+
+// //Terceiro exemplo
+const prompt = require("prompt-sync")();
+
+var nome = prompt("Digite seu nome: ");
+var salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
 var ir = salario_bruto * 0.10;
 var salario_liquido = salario_bruto - ir;
 
-console.log("Salário líquido: R$", salario_liquido);
+console.log("Olá", nome,"! Seu salário líquido é: R$", salario_liquido);
