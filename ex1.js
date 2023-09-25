97e71ac5b83fd3a6517c2fbdfb082886f9a78ead
brunagarcia-almeida <73366397+brunagarcia-almeida@users.noreply.github.com>
2023-09-25 23:25:27 +0000

    Review Comando de Decisão
    Reprograme-se IFES

--- ex1.js
+++ ex1.js
@@ -1,6 +1,18 @@
 //Primeiro exemplo
-var num1 = parseInt("1");
-var num2 = parseFloat("2");
-var num3 = String(3);
+// var num1 = parseInt("1");
+// var num2 = parseFloat("2");
+// var num3 = String(3);
 
-console.log((num1 + num2) + num3);
+// console.log((num1 + num2) + num3);
+
+//Segundo exemplo
+var n1 = 1;
+var n2 = 2;
+var n3 = 3;
+if (n3 % n1 >= 3) {
+  console.log("Entrou no if");
+} else if (n3 % n2 >= 2){
+  console.log("Entrou no else if");
+} else{
+  console.log("Entrou no else");
+}
--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-// Prática exemplo
+// Exemplo - Prática 1
 // var salario_bruto = 7500;
 // var ir = salario_bruto * 0.10;
 // var salario_liquido = salario_bruto - ir;
 
 // console.log("Salário líquido: R$", salario_liquido);
 
-// Prática exemplo
+// Exemplo - Prática 2
+// const prompt = require("prompt-sync")();
+
+// var nome = prompt("Digite seu nome: ");
+// var salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
+// var ir = salario_bruto * 0.10;
+// var salario_liquido = salario_bruto - ir;
+
+// console.log("Olá", nome, "! Seu salário líquido é: R$", salario_liquido);
+
+// Exemplo - Prática 3
 const prompt = require("prompt-sync")();
 
 var nome = prompt("Digite seu nome: ");
 var salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
-var ir = salario_bruto * 0.10;
-var salario_liquido = salario_bruto - ir;
+var ir = null;
+var salario_liquido = null;
 
+if (salario_bruto > 0 && salario_bruto <= 1903.98) {
+  ir = salario_bruto * 0.05;
+} else if (salario_bruto <= 2826.65) {
+  ir = salario_bruto * 0.075;
+} else {
+  ir = salario_bruto * 0.15;
+}
+
+salario_liquido = salario_bruto - ir;
 console.log("Olá", nome, "! Seu salário líquido é: R$", salario_liquido);
